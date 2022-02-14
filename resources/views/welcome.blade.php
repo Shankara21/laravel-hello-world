90841c93b1be3e3317cfdc98230a1f40b6b44ff0
Shankara21 <lazuardit21@gmail.com>
2022-02-14 11:52:40 +0700

    Kode Program Kedua

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -408,7 +408,7 @@
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div class="ml-4 text-lg leading-7 font-semibold">
-            <h1 class="text-gray-900 dark:text-white"> Muhammad Lazuardi Timur</h1>
+            <h1 class="text-gray-900 dark:text-white"> </h1>
         </div>
     </div>
 </body>
